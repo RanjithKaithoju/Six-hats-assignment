99c1a00621497aec19bd9e5d9b1cc0e815074583
unknown <kaithojuranjith@gmail.com>
2021-10-11 12:19:59 +0530

    Added simple validator in serializers for username...

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,7 @@
 #Using default User Model
 from django.contrib.auth.models import User
 from rest_framework import fields, serializers
+from rest_framework.exceptions import ValidationError
 
 class UserSerialiser(serializers.ModelSerializer):
     '''serializer for User Model'''
@@ -9,3 +10,11 @@ class UserSerialiser(serializers.ModelSerializer):
         #fields = '__all__'
         fields = ['id','is_superuser','email','date_joined','username']
         extra_kwargs = {'date_joined':{'required':False}}
+
+        #Simple Inbuilt Validator
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=User.objects.all(),
+                fields=['username']
+            )
+        ]
